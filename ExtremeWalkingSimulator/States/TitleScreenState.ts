ae43e29623a9bf480f5361f1e2dfeb4d7a67d6a4
Serapth <mike@gamefromscratch.com>
2015-01-15 18:45:49 -0500

    Volume change

--- ExtremeWalkingSimulator/States/TitleScreenState.ts
+++ ExtremeWalkingSimulator/States/TitleScreenState.ts
@@ -17,7 +17,7 @@
             this.titleScreenImage.scale.setTo(this.game.width/this.titleScreenImage.width, this.game.height/this.titleScreenImage.height);
             
             this.music = this.game.add.audio("TitleSong");
-            this.music.volume = 100;
+            this.music.volume = 0.25;
             this.music.loop = true;
             this.music.play();
 
