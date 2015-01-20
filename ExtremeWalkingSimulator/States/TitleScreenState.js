f84098723bb041078009f0d4526fa92dbdea49e8
Mike <mike@gamefromscratch.com>
2015-01-19 22:12:07 -0500

    Fgg

--- ExtremeWalkingSimulator/States/TitleScreenState.js
+++ ExtremeWalkingSimulator/States/TitleScreenState.js
@@ -20,7 +20,7 @@ var GameFromScratch;
             this.music = this.game.add.audio("TitleSong");
             this.music.volume = 0.25;
             this.music.loop = true;
-            this.music.play();
+            // comment out to not annoy site visitors this.music.play();
 
             this.input.onTap.addOnce(this.titleClicked, this);
         };
